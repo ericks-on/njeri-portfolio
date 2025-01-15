9b61c4c07a30c9b97e612685d186552ad3d0a118
Erickson Mbuvi <mutisyaerickson@gmail.com>
2025-01-15 18:28:35 +0300

    added contacts

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -9,22 +9,22 @@ export default function Contact() {
             <div className="flex flex-col items-center gap-4">
                 <p className="text-center text-lg">I&apos;m always open to new opportunities and collaborations. Feel free to reach out to me via email or LinkedIn.</p>
                 <div className="flex gap-6">
-                    <Link href="mailto:mutisyaerickson@gmail.com">
+                    <Link href="mailto:mburufaith48@gmail.com">
                         <FaEnvelope className="text-4xl" />
                     </Link>
-                    <Link href="https://www.linkedin.com/in/erickson-mbuvi-data-scientist/" target="blank">
+                    <Link href="www.linkedin.com/in/njeri-mburu-datascientist" target="blank">
                         <FaLinkedinIn className="text-4xl text-blue-500" />
                     </Link>
-                    <Link href="https://www.github.com/ericks-on" target="blank">
+                    <Link href="https://www.github.com/njeri-mb" target="blank">
                         <FaGithub className="text-4xl" />
                     </Link>
-                    <Link href="https://www.instagram.com/erickson_mbuvi" target="blank">
+                    <Link href="https://www.instagram.com/njeri.mburu_" target="blank">
                         <FaInstagram className="text-4xl" />
                     </Link>
                 </div>
                 <div className="flex gap-2 items-center justify-center">
                     <p className="text-4xl">Email:</p>
-                    <p className="text-lg">mutisyaerickson@gmail.com</p>
+                    <p className="text-lg">mburufaith48@gmail.com</p>
                 </div>
             </div>
         </div>
